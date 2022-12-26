7d26ca53b8247eaa568368c9abfc7f50a942f8f4
R.Z.A <53036090+GunHeeKim127@users.noreply.github.com>
2022-12-26 16:27:12 +0900

    22.12.26. file update and site_page_loader

--- 03)_Nodejs_border/README.md
+++ 03)_Nodejs_border/README.md
@@ -14,6 +14,9 @@ npm install mysql
 npm install express-myconnection
 npm install express-handlebars
 npm install body-parser
+npm install path
+npm install morgan
+npm install html
 ```
 1.npm install express : node-express 사용을 위한 설치, 
   express는 rest API를 쓰기 위한 기본적인 환경 제공
@@ -32,5 +35,15 @@ npm install body-parser
 <br>(참조)https://velog.io/@parkoon/%EC%8B%A4%EB%AC%B4%EC%97%90%EC%84%9C-Handlebars-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0-feat-express)
 <br>
 
+5.npm install path : path 모듈은 폴더와 파일의 경로를 지정해주는 모듈이다.
+
+6.npm install morgan : 서버의 로그 관리를 위한 모듈이다.
+
 # gitignore 작성
 1. node-modules, gitattrbuts, package.json
+
+# crud를 위한 폴더 정리
+1. app.js에서 backend폴더의 router폴더 세팅 및 router에서 rest API사용시 app.js에서 불러오기 가능
+2. path를 이용하여 router에서 frontend폴더의 view폴더에 들어있는 html파일 로더 가능
+3. router에 모든 역할을 두는 것이 아닌 controller폴더를 만들어 service 및 controlloer 기능을 가진 파일 저장
+4. app.js에서 public의 파일을 사용할 수 있도록 app.use 세팅
--- 03)_Nodejs_border/app.js
+++ 03)_Nodejs_border/app.js
@@ -2,12 +2,36 @@ const express = require('express');
 const { engine } = require("express-handlebars");
 const myconnection = require("express-myconnection");
 const bodyparser = require("body-parser");
+const path = require('path');
+var logger = require('morgan');
 const mysql = require('mysql');
-
 const app = express();
+
 app.set ('port',4000);
+app.set('views', path.join(__dirname, '/frontend/view/index.html'));
+app.set('view engine', 'html');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/frontend/public')));
+app.use(myconnection(mysql,{
+    host:'localhost',
+    user:'root',
+    password:'0000',
+    port:3600,
+    database:'user_test1'
+}))
 
 app.listen(app.get('port'),()=>
 {
     console.log('listening on port',app.get('port'))
 })
+
+// app.get('/', function(req, res, next) {
+//     res.sendFile(__dirname+'/frontend/view/index.html');
+//   });
+var indexRouter = require('./backend/router/index.js');
+app.use('/', indexRouter);
+
+module.exports = app;
