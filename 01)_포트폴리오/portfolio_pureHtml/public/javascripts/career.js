275ed4c12a22e4d5072b9f0bc111dfc4b739f3a0
김건희 <kimgunhee127@gmail.com>
2025-08-22 17:10:50 +0900

    25.08.22 career 내용 추가 중

--- 01)_포트폴리오/portfolio_pureHtml/public/javascripts/career.js
+++ 01)_포트폴리오/portfolio_pureHtml/public/javascripts/career.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     document.getElementById("career").innerHTML
     ='<object type="text/html" data="./view/career.html" style="width:100%; height:100%;"></object>';
-    document.getElementById("career").style.height="1810px"
+    document.getElementById("career").style.height="2550px"
 })
